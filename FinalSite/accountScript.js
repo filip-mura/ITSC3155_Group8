b15194e25fce48a921679aadccdff057de8659c9
filip-mura <fmura@uncc.edu>
2022-05-02 13:44:22 -0400

    Pushing update

---
+++ FinalSite/accountScript.js
@@ -0,0 +1,8 @@
+function hideandshow() {
+    var x = document.getElementById("passwrd");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
